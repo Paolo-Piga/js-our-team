bd97aeaf29b567beb86dc8b7685d5e9f028c3dec
DecimTheJudge <paolo1piga@gmail.com>
2024-06-18 15:53:45 +0200

    continuo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // creo l'array di oggetti 
+
 const team = {
     "nome" : [`Wayne Barnett`, `Angela Caroll`,`Walter Gordon`,`Angela Lopez`,`Scott Estrada`,`Barbara Ramos`],
     "ruolo" : [`Founder & CEO`,`Chief Editor`,`Office Manager`,`Social Media Manager`,`Developer`,`Graphic Designer`],
-    "foto" : []
+    "foto" : `img\wayne-barnett-founder-ceo.jpg`
 };
