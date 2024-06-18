4d922ff646206a02852050ae1846281d92711026
DecimTheJudge <paolo1piga@gmail.com>
2024-06-18 15:44:38 +0200

    inizio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// creo l'array di oggetti 
+const team = {
+    "nome" : [`Wayne Barnett`, `Angela Caroll`,`Walter Gordon`,`Angela Lopez`,`Scott Estrada`,`Barbara Ramos`],
+    "ruolo" : [`Founder & CEO`,`Chief Editor`,`Office Manager`,`Social Media Manager`,`Developer`,`Graphic Designer`],
+    "foto" : []
+};
